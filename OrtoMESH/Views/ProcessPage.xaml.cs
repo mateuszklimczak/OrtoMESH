66c288c74b13deb609f7ef5df47fc98d3910e500
mateuszklimczak <mat.klimczak1@gmail.com>
2017-03-18 00:09:06 +0100

    ...

--- OrtoMESH/Views/ProcessPage.xaml.cs
+++ OrtoMESH/Views/ProcessPage.xaml.cs
@@ -7,6 +7,11 @@ namespace OrtoMESH
 {
 	public partial class ProcessPage : ContentPage
 	{
+		void Confirm_Clicked(object sender, System.EventArgs e)
+		{
+			DisplayAlert("New patient","New patient has been added","OK");
+		}
+
 		void BackButton_Clicked(object sender, System.EventArgs e)
 		{
 			Navigation.PopModalAsync();
